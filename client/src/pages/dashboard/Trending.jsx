affe46fbe88da099eb38de7ca2e85e1deefeae6e
Elya <elyaallahyari@gmail.com>
2025-09-21 12:36:22 +0330

    Trending skills Dashboard page

--- client/src/pages/dashboard/Trending.jsx
+++ client/src/pages/dashboard/Trending.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import chart_1 from '../../assets/images/chart_1.svg'
+import chart_2 from '../../assets/images/chart_2.svg'
+import chart_3 from '../../assets/images/chart_3.svg'
+import chart_4 from '../../assets/images/chart_4.svg'
+import '../../assets/styles/Trending.css'
 
 const Trending = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="trending">
+        <div className="trending__head">
+          <img src={chart_1} />
+          <img src={chart_2} />
+          <img src={chart_3} />
+        </div>
+        <div className="trending__foot">
+          <img src={chart_4} />
+        </div>
+      </div>
+    </>
   )
 }
 
