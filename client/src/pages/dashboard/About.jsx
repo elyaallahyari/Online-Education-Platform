5710d9c5d0cf35ab295e2cd48b6c91989ff9fdcf
Elya <elyaallahyari@gmail.com>
2025-09-30 23:44:57 +0330

    About page

--- client/src/pages/dashboard/About.jsx
+++ client/src/pages/dashboard/About.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="about">
+        <div className="about__title">
+          <h2>World-Class Learning for Anyone, Anywhere</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
